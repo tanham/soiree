bc3c403fd58a636e9f22e07366d56b8e932a784c
Tanisha <tanishahampden@gmail.com>
2018-01-04 10:38:46 -0800

    add reusable card component and styling

--- src/components/DateDetail.js
+++ src/components/DateDetail.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text} from 'react-native';
+import Card from './Card';
 
 const DateDetail = (props) => {
   return (
-    <View>
+    <Card>
       <Text>{props.date.fields.Name}</Text>
-    </View>
+    </Card>
   );
 };
 
