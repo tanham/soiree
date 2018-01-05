102ca744a12415241c111b790ca5a0f710fe5e5c
Tanisha <tanishahampden@gmail.com>
2018-01-05 10:31:38 -0800

    expo conversion

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import Header from './src/components/Header';
 import PageHeader from './src/components/PageHeader';
 
 import App from './App';
+import 'expo';
 
 // render App.js to the device
 AppRegistry.registerComponent('soiree', () => App);
+AppRegistry.registerComponent('main', () => App);
