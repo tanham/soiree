a979d413feab97faa911bed5c977ac3d7b0c64ec
Tanisha <tanishahampden@gmail.com>
2018-01-05 11:16:07 -0800

    add conditional rendering

--- App.js
+++ App.js
@@ -14,21 +14,26 @@ import DatesList from './src/components/DatesList';
 const App = () => {
 
   return (
+    // <View>
+    // {this.state.page === 1 &&
+    //   <View>
+    //     <Header headerText={'Soiree'} />
     <ScrollView>
-      <Header headerText={'Soiree'} />
+      {this.state.page === 1 &&
+        <Header headerText={'Soiree'} />
 
-      <Button
-        // TODO: create a helper funtion to call in onPress
+        <Button
+          // TODO: create a helper funtion to call in onPress
 
-        onPress={this.console.log("pressed the button")}
-        title="Find Dates"
-        color="#841584"
-      />
+          onPress={this.console.log("pressed the button")}
+          title="Find Dates"
+          color="#841584"
+        />
 
-      <PageHeader pageHeaderText={'Featured Dates'} />
-      <DatesList />
-    </ScrollView>
-  );
-};
+        <PageHeader pageHeaderText={'Featured Dates'} />
+        <DatesList />
+      </ScrollView>
+    );
+  };
 
 export default App;
