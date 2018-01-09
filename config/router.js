9921341adcb626c278cc4bfe92bb17be8fc2a688
Tanisha <tanishahampden@gmail.com>
2018-01-09 10:23:06 -0800

    trying to make it work

--- config/router.js
+++ config/router.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { TabNavigator } from 'react-navigation';
+import { TabNavigator, StackNavigator } from 'react-navigation';
 import { Icon } from 'react-native-elements';
 import Home from '../src/screens/Home';
 import Results from '../src/screens/Results';
 
 export const Tabs = TabNavigator({
   Home: {
-    screen: Home,
+    screen: 'Home',
   },
   Results: {
-    screen: Results,
+    screen: 'Results',
   },
 });
