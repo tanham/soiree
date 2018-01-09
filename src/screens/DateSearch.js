c7d6ac6a27cda01086e5d4f7d3f52048efc1f428
Tanisha <tanishahampden@gmail.com>
2018-01-09 14:23:55 -0800

    add form

--- src/screens/DateSearch.js
+++ src/screens/DateSearch.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import Header from '../components/Header';
+import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements';
 
 export default class DateSearch extends React.Component {
+  someFunction() {
+    console.log('doing the form thing');
+  };
   render() {
+
     return (
 
       <View>
         <Header headerText={'Soiree'} />
         {this.props.children}
-        <Text>Stuff</Text>
+        <FormLabel>Where You At?</FormLabel>
+        <FormInput onChangeText={this.someFunction}/>
+        <FormValidationMessage>{'This field is required'}</FormValidationMessage>
+
+        <Button
+          onPress={this.someFunction}
+          title="Submit"
+        />
       </View>
     );
   }
