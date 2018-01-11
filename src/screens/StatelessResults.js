2d61d2bca37214293d32143656aebcd0446ccf15
Tanisha <tanishahampden@gmail.com>
2018-01-11 11:14:03 -0800

    add stateless results page

---
+++ src/screens/StatelessResults.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { ScrollView, Text } from 'react-native';
+import { Tile, List, ListItem, Button } from 'react-native-elements';
+import { Tile, List, ListItem, Button } from 'react-native-elements';
+
+class StatelessResults extends Component {
+  render() {
+    return (
+      Text>Results Page</Text>
+
+    );
+  }
+}
+
+export default StatelessResults;
