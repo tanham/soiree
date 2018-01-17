176de51b9a8521e641471986fc31169d184beb5f
Tanisha <tanishahampden@gmail.com>
2018-01-17 13:01:37 -0800

    add lists category boxes to homepage

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,14 +1,72 @@
 import React, { Component } from 'react';
-import { ScrollView, View } from 'react-native';
+import { ScrollView, Text, View, TouchableOpacity } from 'react-native';
 import { Tile, List, ListItem, Button } from 'react-native-elements';
 import Header from '../components/Header';
 import DatesList from '../components/DatesList';
+import CustomButton from '../components/CustomButton';
+import CardSection from '../components/Card';
 
 class Home extends Component {
   render() {
     return (
       <ScrollView>
-        
+        <Text style={{textAlign: 'center'}}>
+        Featued Lists</Text>
+
+        <View style={{flexDirection: 'row', }}>
+          <CardSection>
+            <TouchableOpacity onPress={() =>console.log("it's happening")}>
+              <Text style={{textAlign: 'center', // <-- the magic
+                            fontWeight: 'bold',
+                            fontSize: 48,
+                            marginTop: 100,
+                            }}>
+              Eats
+              </Text>
+            </TouchableOpacity>
+          </CardSection>
+
+          <CardSection>
+            <TouchableOpacity onPress={() =>console.log("it's happening")}>
+              <Text style={{textAlign: 'center', // <-- the magic
+                            fontWeight: 'bold',
+                            fontSize: 48,
+                            marginTop: 100,}}>
+              Active
+              </Text>
+            </TouchableOpacity>
+          </CardSection>
+        </View>
+
+        <View style={{flexDirection: 'row'}}>
+        <CardSection>
+        <TouchableOpacity onPress={() =>console.log("it's happening")}>
+          <Text style={{textAlign: 'center', // <-- the magic
+                        fontWeight: 'bold',
+                        fontSize: 48,
+                        marginTop: 100,}}>
+          Live
+          </Text>
+        </TouchableOpacity>
+        </CardSection>
+
+
+        <CardSection>
+        <TouchableOpacity onPress={() =>console.log("it's happening")}>
+          <Text style={{textAlign: 'center', // <-- the magic
+                        fontWeight: 'bold',
+                        fontSize: 48,
+                        marginTop: 100,}}>
+          Random
+          </Text>
+        </TouchableOpacity>
+        </CardSection>
+        </View>
+
+
+
+
+
         <DatesList />
       </ScrollView>
 
