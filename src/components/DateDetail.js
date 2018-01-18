8870299b382d0faf5bfcbdd0ed03393d7b212823
Tanisha <tanishahampden@gmail.com>
2018-01-18 13:35:02 -0800

    change from funcitonal to class based component so I have access to render

--- src/components/DateDetail.js
+++ src/components/DateDetail.js
@@ -1,34 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 import CustomButton from './CustomButton';
 
-const DateDetail = (props) => {
+class DateDetail extends Component {
 
-  return (
-    <Card>
-      <CardSection>
-        <View style={styles.headerContentStyle}>
-          <Text style={styles.headerTextStyle}>{props.date.fields.Name}</Text>
-          <Text>Some location</Text>
-        </View>
-      </CardSection>
-      <CardSection>
-        <Image
-          style={styles.imageStyle}
-          source={{ uri : 'http://clipartix.com/wp-content/uploads/2016/04/Hearts-heart-clipart.png'}}
-        />
-      </CardSection>
-      <CardSection>
-        <CustomButton onPress={() => console.log(props.date.fields.Name)}>
-          Click Me
-        </CustomButton>
-      </CardSection>
+  render(props) {
+    return (
+      <Card>
+        <CardSection>
+          <View style={styles.headerContentStyle}>
+            <Text style={styles.headerTextStyle}>{this.props.date.fields.Name}</Text>
+            <Text>Some location</Text>
+          </View>
+        </CardSection>
+        <CardSection>
+          <Image
+            style={styles.imageStyle}
+            source={{ uri : 'http://clipartix.com/wp-content/uploads/2016/04/Hearts-heart-clipart.png'}}
+          />
+        </CardSection>
+        <CardSection>
+          <CustomButton onPress={() => console.log(props.date.fields.Name)}>
+            Click Me
+          </CustomButton>
+        </CardSection>
 
-    </Card>
-  );
-};
+      </Card>
+    );
+  }
+}
 
 const styles = {
   headerContentStyle: {
